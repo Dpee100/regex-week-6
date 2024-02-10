de90e54b98e0b0025de76f7ac1a1e530e235b25e
Dpee100 <126196408+Dpee100@users.noreply.github.com>
2024-02-10 09:59:14 +0100

    Update regex.js

--- regex.js
+++ regex.js
@@ -8,6 +8,7 @@ console.log(masterCard.test("2230-1807-3345-7805"));
 const verveCard = /^((506099)|(5061[0-8][0-9])|(50619[0-8])|(65000[2-9])|(65001[0-9])|(65002[0-7])|(50786[5-9])|(5078[7-9][0-9])|(5079[0-5][0-9])|(50796[0-4]))(\d{16})|(\d{18,19})$/;
 console.log(verveCard.test("5061030000000000050"));
 
+//Regex to match a visa card
 // Visa has 13 and 16 digits, it starts with 4.
 const visaCard = /^4(\d{13})|(\d{16})$/;
 console.log(visaCard.test("4263982640269299"));
